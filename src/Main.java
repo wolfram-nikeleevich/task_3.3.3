8456a63d0c38c1361bc25a2ced85bfcb0e29ed6f
Владимир Сизов <7580844@mail.ru>
2022-02-19 19:04:27 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        double amount = 1_000_000;
+        double[] period = new double[] {12,24,36};
+        double[] mounthPayment = new double[3];
+        for(int i = 0;i < 3;i++) {
+            mounthPayment[i] = service.calculate(amount, period[i]);
+            System.out.println(mounthPayment[i]);
+        }
+    }
+}
