a8e757cd49a1752518216db027aa94f410197455
Владимир Сизов <7580844@mail.ru>
2022-02-27 11:00:06 +0300

    code reformated

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public double calculate(double amount, double period) {
         double percentageRate = 9.99;
-        double i = percentageRate/1200;
-        double k = i/(Math.pow(1+i,period) - 1) + i;
-        return k*amount;
+        double i = percentageRate / 1200;
+        double k = i / (Math.pow(1 + i, period) - 1) + i;
+        return k * amount;
     }
 }
